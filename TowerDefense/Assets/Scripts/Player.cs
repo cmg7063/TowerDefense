2fc9a9a14cdc1b10896715ec381bbf39470e1b31
zrm2647 <zrm2647@g.rit.edu>
2018-04-12 17:51:47 -0400

    Placing towers (including cycle and scrap)
    "e" places tower, "tab" cycles. each tower uses 50 scrap, can't build if less than

--- TowerDefense/Assets/Scripts/Player.cs
+++ TowerDefense/Assets/Scripts/Player.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float speed;
+    public GameObject tower1, tower2, tower3;
+    private GameObject[] towers;
+    private GameObject towerCurrent;
+    private int towerSelect;
+    public int scrap;
 
 	// Use this for initialization
 	void Start () {
         speed = 3.0f;
+        towers = new GameObject[3];
+        towers[0] = tower1;
+        towers[1] = tower2;
+        towers[2] = tower3;
+        towerCurrent = tower1;
+        towerSelect = 0;
 	}
 	
 	// Update is called once per frame
@@ -28,6 +39,29 @@ public class Player : MonoBehaviour {
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            towerSelect += 1;
+            if(towerSelect >= towers.Length)
+            {
+                towerSelect = 0;
+            }
+            towerCurrent = towers[towerSelect];
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            PlaceTower();
+        }
+    }
+
+    void PlaceTower()
+    {
+        if (scrap >= 50)
+        {
+            Instantiate(towerCurrent, this.transform.position, this.transform.rotation);
+            scrap -= 50;
         }
     }
 }
