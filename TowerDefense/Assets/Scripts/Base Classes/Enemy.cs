773ae45578e1d2956b16d4e1598d8b8f88225345
Corinne Green <cmg7063@g.rit.edu>
2018-04-12 21:18:12 -0400

    enemy base class complete, basic menu UI setup
    Play on menu linked to Sungmin's test scene

---
+++ TowerDefense/Assets/Scripts/Base Classes/Enemy.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+
+    private EnemyState state = EnemyState.Wander;
+
+    public float minDistance;
+    public float maxDistance;
+
+    public float speed;
+
+    public enum EnemyState
+    {
+        Wander,
+        Pursue,
+        Stop
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 vecToPlayer = (player.transform.position - transform.position);
+        float distance = Vector2.Distance(player.transform.position, transform.position);
+
+        float angle = Mathf.Atan2(vecToPlayer.y, vecToPlayer.x) * Mathf.Rad2Deg - 90f;
+        Quaternion quat = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, quat, 180);
+
+        if (distance > minDistance && distance < maxDistance)
+        {
+            state = EnemyState.Pursue;
+        }
+        else if(distance > maxDistance)
+        {
+            state = EnemyState.Wander;
+        }
+        else if (distance > 0)
+        {
+            state = EnemyState.Stop;
+        }
+
+        // State logic
+        if (state == EnemyState.Pursue)
+        {
+            Debug.Log("Pursuing!");
+            speed = 2.0f;
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        if (state == EnemyState.Wander)
+        {
+            Debug.Log("Wandering!");
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, quat, 180);
+        }
+        if(state == EnemyState.Stop)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, quat, 180);
+        }
+
+        //float angle = Mathf.Atan2(vecToPlayer.y, vecToPlayer.x) * Mathf.Rad2Deg - 90f;
+        //Quaternion quat = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, quat, 180);
+        //transform.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "TowerBullet")
+        {
+            Debug.Log("Colliding with bullet");
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ TowerDefense/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MenuManager : MonoBehaviour
+{
+    public Button instructions;
+    public Button play;
+    public Button quit;
+    public Button close;
+    public Button credits;
+    public Button menu;
+
+    private GameObject instructionPane;
+
+    private bool instructionsClicked;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Menu")
+        {
+            instructionPane = GameObject.FindGameObjectWithTag("ShowOnClick");
+            hideText();
+        }
+    }
+
+    void hideText()
+    {
+        instructionPane.SetActive(false);
+    }
+
+    void showText()
+    {
+        instructionPane.SetActive(true);
+    }
+
+    public void Instructions()
+    {
+        showText();
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+
+    public void Close()
+    {
+        hideText();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("SungTest");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
