9b84b4afd10eec643bb99c62c761eabca34085ad
Sungmin Park <sp4216@rit.edu>
2018-05-06 16:52:58 -0400

    update lightning trap

---
+++ TowerDefense/Assets/Scripts/Trap Scripts/LightningScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningScript : MonoBehaviour {
+	public Sprite[] animations;
+
+	private int currentAnimation;
+	private int frameCounter;
+
+	// Use this for initialization
+	void Start () {
+		currentAnimation = 0;
+		frameCounter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (frameCounter % 20 == 0) {
+			currentAnimation++;
+			if (currentAnimation >= animations.Length) {
+				currentAnimation = 0;
+			}
+
+			gameObject.GetComponent<SpriteRenderer>().sprite = animations[currentAnimation];
+		}
+
+		frameCounter++;
+		if (frameCounter >= 60) {
+			frameCounter = 0;
+		}
+	}
+}
