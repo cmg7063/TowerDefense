419d397f06a3528750d16b7bb20bfbc50c7e0897
zrm2647 <zrm2647@g.rit.edu>
2018-05-06 17:18:00 -0400

    final score added to game over screen

---
+++ TowerDefense/Assets/Scripts/FinalScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalScore : MonoBehaviour {
+
+    public Text fScoreText;
+    public static int fScore;
+
+    // Use this for initialization
+    void Start () {
+        fScoreText.text = "FINAL SCORE: ";
+        fScore = PlayerPrefs.GetInt("Score");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fScoreText.text = "FINAL SCORE: " + fScore;
+	}
+}
